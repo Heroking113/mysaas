e01dd50fe7877068120395aa27f13c45f878aff2
Heroking113 <1819785416@qq.com>
2020-06-28 23:00:33 +0800

    push

--- config/prod.py
+++ config/prod.py
@@ -29,8 +29,8 @@ DATABASES.update(
     {
         'default': {
             'ENGINE': 'django.db.backends.mysql',
-            'NAME': '2017222072_prod_herokingfsaas',  # 数据库名
-            'USER': '1819785416',  # 数据库用户
+            'NAME': '2017222072_herokingfsaas',  # 数据库名
+            'USER': 'root',  # 数据库用户
             'PASSWORD': 'Heroking113.',  # 数据库密码
             'HOST': '10.0.2.8',  # 数据库主机
             'PORT': '3306',  # 数据库端口
